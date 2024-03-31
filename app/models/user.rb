7cb82b4f077e87b5b6f317c7850cdedac8965826
DESKTOP-30E128T\masan <youhongzhongjing@gmail.com>
2024-03-31 16:03:28 +0900

    画像アップロードと表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,15 +35,15 @@ class User < ApplicationRecord
     profile&.nickname || self.email.split('@').first
   end
 
-  # def birthday
-  #   profile&.birthday
-  # end
-
-  # def gender
-  #   profile&.gender
-  # end
-
   def prepare_profile
     profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 end
