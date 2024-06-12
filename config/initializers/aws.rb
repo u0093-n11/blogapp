6f694c15d53106bee35e0160ef128bbf381108ba
DESKTOP-30E128T\masan <youhongzhongjing@gmail.com>
2024-06-12 23:12:36 +0900

    aws.rbとDEFAULT_CERT_FILEを削除した

--- config/initializers/aws.rb
+++
@@ -1,8 +0,0 @@
-require 'aws-sdk'
-
-Aws.config.update(
-  region: Rails.configuration.aws_region,
-  credentials: Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_secret_access_key)
-)
-
-Aws.use_bundled_cert!
